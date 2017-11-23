e1507983d9e5ab5feebecbaf3ce78232fd1754bf
Oetzie <info@oetzie.nl>
2017-11-23 22:35:28 +0100

    Typo's updated

--- core/components/domains/lexicon/en/default.inc.php
+++ core/components/domains/lexicon/en/default.inc.php
@@ -24,7 +24,7 @@
     
     $_lang['area_domains']                                              = 'Domain names';
     
-    $_lang['setting_domains.branding_url'                               = 'Branding';
+    $_lang['setting_domains.branding_url']                              = 'Branding';
     $_lang['setting_domains.branding_url_desc']                         = 'The URL of the branding button, if the URL is empty the branding button won\'t be shown.';
     $_lang['setting_domains.branding_url_help']                         = 'Branding (help)';
     $_lang['setting_domains.branding_url_help_desc']                    = 'The URL of the branding help button, if the URL is empty the branding help button won\'t be shown.';
@@ -54,16 +54,16 @@
     $_lang['domains.label_active_desc']                                 = '';
     
     $_lang['domains.online']                                            = 'Online';
-	$_lang['domains.offline']                                           = 'Offline';
-	$_lang['domains.primary_domain']                                    = 'This domain name is the primary domain for this context and overrules the physical context settings.';
-	$_lang['domains.error_site_start']                                  = 'The selected startpage does not exists. Define another startpage.';
-	$_lang['domains.error_site_start_context']                          = 'The selected startpage doest not exists in the current context. Define another startpage..';
-	$_lang['domains.error_site_start_deleted']                          = 'The selected startpage is deleted. Define another startpage.';
-	$_lang['domains.error_site_start_published']                        = 'The selected startpagina is not published. Define another startpage.';
-	$_lang['domains.error_site_error']                                  = 'The selected 404 page does not exists. Define another 404 page.';
-	$_lang['domains.error_site_error_context']                          = 'The selected 404 page doest not exists in the current context. Define another 404 page..';
-	$_lang['domains.error_site_error_deleted']                          = 'The selected 404 page is deleted. Define another 404 page.';
-	$_lang['domains.error_site_error_published']                        = 'The selected 404 page is not published. Define another 404 page.';
-	$_lang['domains.error_remove_primary']                              = 'This domain name is the primary domain for a context and can not be removed.';
+    $_lang['domains.offline']                                           = 'Offline';
+    $_lang['domains.primary_domain']                                    = 'This domain name is the primary domain for this context and overrules the physical context settings.';
+    $_lang['domains.error_site_start']                                  = 'The selected startpage does not exists. Define another startpage.';
+    $_lang['domains.error_site_start_context']                          = 'The selected startpage doest not exists in the current context. Define another startpage..';
+    $_lang['domains.error_site_start_deleted']                          = 'The selected startpage is deleted. Define another startpage.';
+    $_lang['domains.error_site_start_published']                        = 'The selected startpagina is not published. Define another startpage.';
+    $_lang['domains.error_site_error']                                  = 'The selected 404 page does not exists. Define another 404 page.';
+    $_lang['domains.error_site_error_context']                          = 'The selected 404 page doest not exists in the current context. Define another 404 page..';
+    $_lang['domains.error_site_error_deleted']                          = 'The selected 404 page is deleted. Define another 404 page.';
+    $_lang['domains.error_site_error_published']                        = 'The selected 404 page is not published. Define another 404 page.';
+    $_lang['domains.error_remove_primary']                              = 'This domain name is the primary domain for a context and can not be removed.';
 	
 ?>
